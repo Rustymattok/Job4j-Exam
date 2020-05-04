1c339461807797088b579f970db0a98af80fdfdb
Rustymattok <mrArxi@gmail.com>
2020-05-04 16:41:00 +0300

    add style for buttons

---
+++ app/src/main/java/ru/job4j/model/Question.java
@@ -0,0 +1,49 @@
+package ru.job4j.model;
+
+import java.util.List;
+
+public class Question {
+    private int id;
+    private String text;
+    private List<Option> options;
+    private int answer;
+
+    public Question(int id, String text, List<Option> options, int answer) {
+        this.id = id;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
